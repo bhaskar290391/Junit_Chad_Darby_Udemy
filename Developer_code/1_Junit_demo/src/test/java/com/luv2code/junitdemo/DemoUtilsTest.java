f1654d7f6b8c2d196cb1bb15103314bf500e971c
bhaskar290391 <mudaliyarbhaskar29@gmail.com>
2024-06-04 01:10:18 +0530

    junit assert

---
+++ Developer_code/1_Junit_demo/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -0,0 +1,72 @@
+package com.luv2code.junitdemo;
+
+import org.junit.jupiter.api.*;
+
+import static  org.junit.jupiter.api.Assertions.*;
+
+//@DisplayNameGeneration(DisplayNameGenerator.Simple.class)
+//@DisplayNameGeneration(DisplayNameGenerator.IndicativeSentences.class)
+//@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class DemoUtilsTest {
+
+    private DemoUtils demo;
+
+    @BeforeEach
+    public void beforeTestExecution(){
+        demo=new DemoUtils();
+        System.out.println("Executing Before each for Each Execution method ");
+    }
+
+    @AfterEach
+    public void afterEachExecution(){
+        System.out.println("Executing After each for Each Execution method \n");
+    }
+
+    @BeforeAll
+    static  void beforeAllForClass(){
+        System.out.println("Executing Before all for Each Class \n");
+    }
+
+    @AfterAll
+    static  void afterAllForClass(){
+        System.out.println("Executing After all for Each Class \n");
+    }
+
+    @Test
+    @DisplayName("Equals and Not Equals")
+    public void assertEqualAndNotEqual(){
+        System.out.println("Running Test ==> assertEqualAndNotEqual()");
+
+        assertEquals(6,demo.add(2,4),"2 and 4 must be 6");
+        assertNotEquals(6,demo.add(2,2),"2 and 2 must not be 6");
+    }
+
+
+    @Test
+    @DisplayName("Null and Not Nulls")
+    public void checkNullAndNotNull(){
+        System.out.println("Running Test ==> checkNullAndNotNull()");
+
+        String str=null;
+        String str2="bhaksar";
+        assertNull(demo.checkNull(str),"Given string is null");
+        assertNotNull(demo.checkNull(str2),"Given string is not null");
+    }
+
+
+    @Test
+    @DisplayName("Same and Not Same")
+    public void sameAndNotSame(){
+        String str="bhaskar";
+
+        assertSame(demo.getAcademy(),demo.getAcademyDuplicate(),"This should match");
+        assertNotSame(str,demo.getAcademyDuplicate(),"This should not match");
+    }
+
+    @Test
+    @DisplayName("True Or False")
+    public void trueAndFalse(){
+        assertTrue(demo.isGreater(10,5),"This should return true");
+        assertFalse(demo.isGreater(5,10),"This should return false");
+    }
+}
